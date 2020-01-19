d44e8085e5347ba5f47300808111a84dd65fc91a
Andreaierardi <andreierardi@gmail.com>
2020-01-19 15:22:44 +0100

    Code reordering

--- R/milanpollution.R
+++ R/milanpollution.R
@@ -7,38 +7,16 @@
 #    http://shiny.rstudio.com/
 #
 
+
 milanpollution <- function() {
-    library(shiny)
-    library(ggplot2)
-    library(forecast)
-    library(xts)
-    library(ckanr)
-    library(httr)
-    library(jsonlite)
-    library(tidyverse)
 
 
-    url <- paste0("http://dati.comune.milano.it/api/action/",
-                  "datastore_search?",
-                  "resource_id=698a58e6-f276-44e1-92b1-3d2b81a4ad47&limit=10000")
-    page <- GET(url) # API request
-    status_code(page) # # Check that the call is successful
-    leggo_list <- fromJSON(url)
-    leggo <- leggo_list$result$records
-
-    Data = leggo
-    Data$stazione_id=NULL
-    Data$`_id`=NULL
-    Data$valore = as.double(Data$valore)
-    Data$inquinante = as.factor(Data$inquinante)
-    Data = Data[complete.cases(Data),]
-    Data$data = as.Date(Data$data)
-    test = aggregate(valore~ data+inquinante, Data , mean)
-
-
-
+    loadlibreries()
+    test = datacleaning()
     # Define UI for application that draws a histogram
-
+    header <- dashboardHeader(
+        title = "Milan pollution 2019"
+    )
     ui <- fluidPage(
         titlePanel("Milan pollution 2019"),
         sidebarLayout(
@@ -55,16 +33,22 @@ milanpollution <- function() {
                             "Forecasting lags:",
                             min = 1,  max = 50, value = 1),
             ),
+
+
+
+
             mainPanel(
+
                 plotOutput("BarPlot"),
                 plotOutput("Timeseries"),
                 plotOutput("Forecast")
 
-            )
+            ),
+
+
         )
     )
-
-    # Define server logic required to draw a histogram
+    # Define server logic required to draw plots
     server <- function(input, output) {
 
 
@@ -75,9 +59,7 @@ milanpollution <- function() {
             poll= poll[,c('data','valore')]
             ggplot(poll, aes(x=data,y=valore, group=1))+geom_point()+geom_line()+ geom_smooth(method="lm")
 
-
-
-            # Render a barplot
+            #Render plot data on x and value on y
         })
 
 
@@ -91,13 +73,12 @@ milanpollution <- function() {
             time = poll$data = NULL
             time.ts = as.ts(poll)
             plot(time.ts)
-            # Render a barplot
+            # Render timeseries plot
 
         })
 
         output$Forecast<- renderPlot({
             inp = input$pollutant
-
             poll = subset(test,subset= inquinante==inp)
             poll= poll[,c('data','valore')]
             ggplot(poll, aes(x=data,y=valore, group=1, color=valore))+geom_point()+geom_line()+ geom_smooth(method="lm")
@@ -115,4 +96,40 @@ milanpollution <- function() {
     # Run the application
     shinyApp(ui = ui, server = server)
 }
+
+loadlibreries <- function()
+{
+    library(shiny)
+    library(ggplot2)
+    library(forecast)
+    library(xts)
+    library(ckanr)
+    library(httr)
+    library(jsonlite)
+    library(tidyverse)
+    library(shinydashboard)
+    library(leaflet)
+
+}
+
+datacleaning <- function()
+{
+    url <- paste0("http://dati.comune.milano.it/api/action/",
+                  "datastore_search?",
+                  "resource_id=698a58e6-f276-44e1-92b1-3d2b81a4ad47&limit=10000")
+    page <- GET(url) # API request
+    status_code(page) # # Check that the call is successful
+    leggo_list <- fromJSON(url)
+    leggo <- leggo_list$result$records
+
+    Data = leggo
+    Data$stazione_id=NULL
+    Data$`_id`=NULL
+    Data$valore = as.double(Data$valore)
+    Data$inquinante = as.factor(Data$inquinante)
+    Data = Data[complete.cases(Data),]
+    Data$data = as.Date(Data$data)
+    test = aggregate(valore~ data+inquinante, Data , mean)
+}
+milanpollution()
 
