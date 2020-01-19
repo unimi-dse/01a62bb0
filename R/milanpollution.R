669b20913cf031594b2a180f5a0581727028f4d7
Andreaierardi <andreierardi@gmail.com>
2020-01-19 23:13:44 +0100

    analysis also for year 2017 and 2018

--- R/milanpollution.R
+++ R/milanpollution.R
@@ -7,29 +7,35 @@
 #    http://shiny.rstudio.com/
 #
 
-#For Make a README
+#Make a README
 #https://www.makeareadme.com/
+
 
 
 milanpollution <- function() {
 
-
-
     loadlibreries()
-    filerd= scraping()
-    test = datacleaning(filerd)
+
+    flat_ds2019= scraping("698a58e6-f276-44e1-92b1-3d2b81a4ad47")
+    ds2019 = datacleaning(flat_ds2019)
+    flat_ds2018 = scraping("ea80c691-74bd-4356-94b6-0f446f190c0b")
+    ds2018 = datacleaning(flat_ds2018)
+    flat_ds2017= scraping("a032a06e-24c2-4df1-ac83-d001e9ddc577")
+    ds2017 =datacleaning(flat_ds2017)
     url = a("Comune di Milano", href="https://dati.comune.milano.it/dataset")
+
+    test =ds2019
 
     # Define UI for application
     ui <- fluidPage(
 
 
 
-        titlePanel("Milan pollution 2019"),
-
+        titlePanel(  h1(id="big-heading", "Milan Pollution Analisys")),
+        tags$style(HTML("#big-heading{ color: #7c795d; font-family: 'Raleway',sans-serif; font-size: 40px; font-weight: 600; line-height: 27px; margin: 0 0 10px; text-align: center; }")),
 
         navbarPage("Pollution Milan",
-                   tabPanel("Pollution 2019",
+                   tabPanel("Pollution",
 
         sidebarLayout(
             sidebarPanel(
@@ -38,7 +44,6 @@ milanpollution <- function() {
                             label = "Select pollutant type",
                             choices = unique(test$inquinante),
                             selected = "PM10"),
-
                  # Default selection
                 sliderInput("lag",
                             "Forecasting lags:",
@@ -46,9 +51,15 @@ milanpollution <- function() {
 
 
                 hr(),
-                checkboxInput("regression", "Show Regression line", FALSE),
+                checkboxInput("regression", "Show Regression line", TRUE),
                 hr(),
                 helpText("Data from openData",url),
+                hr(),
+                br(),
+                selectInput("years",
+                            label = "Select the year",
+                            choices = c("2019","2018","2017"),
+                            selected = "2019"),
 
 
             ),
@@ -67,6 +78,8 @@ milanpollution <- function() {
                  sidebarLayout(
                      sidebarPanel(
                          helpText("Data from openData",url),
+
+
                      ),
 
                  mainPanel(
@@ -76,37 +89,42 @@ milanpollution <- function() {
                  ))
 
                  ),
-        tabPanel("Pollution 2017")
+        tabPanel("About")
         ),
     )
+
     # Define server logic required to draw plots
     server <- function(input, output) {
 
 
         output$stations_plot <- renderPlot(
         {
-           df =  stazioni_clean(filerd)
-           df
 
+           flat =  checkyears(input$years, TRUE)
+           df =  stazioni_clean(flat)
            ggplot(df, aes(x=station_id, y=total_detected, fill=station_id)) +
-               geom_bar(stat="identity",color="black")+theme_minimal()+theme(plot.title = element_text(size=18, face="bold"))+ggtitle("Number of detected datas from each station")
+           geom_bar(stat="identity",color="black")+theme_minimal()+theme(plot.title = element_text(size=18, face="bold"))+ggtitle(paste("Number of detected datas from each station year:",input$years))
 
         })
         output$Timeseries <- renderPlot({
 
+            test = checkyears(input$years,FALSE)
+
             if(input$regression)
             {
+
                 inp = input$pollutant
 
                 poll = subset(test,subset= inquinante==inp)
                 poll= poll[,c('data','valore')]
-                ggplot(poll, aes(x=data,y=valore, group=1))+geom_point()+geom_line()+ geom_smooth(method="lm")+theme_minimal()
+                ggplot(poll, aes(x=data,y=valore, group=1),)+geom_line(color = "#00AFBB",size=1)+ geom_smooth(method="lm")+theme_minimal()+theme(plot.title = element_text(size=16, face="bold"))+ggtitle(paste("Time series+regression year:",input$years," of ",inp))
                 #Render plot data on x and value on y
 
             }
 
             else
                 {
+
                     inp = input$pollutant
 
                     poll = subset(test,subset= inquinante==inp)
@@ -114,7 +132,7 @@ milanpollution <- function() {
 
                     time = poll$data = NULL
                     time.ts = as.ts(poll)
-                    plot(time.ts)+ geom_smooth(method="lm")
+                    plot(time.ts,main=paste("Time series year:",input$years," of ",inp))+ geom_smooth(method="lm")
                     # Render timeseries plot
 
             }
@@ -122,6 +140,9 @@ milanpollution <- function() {
         })
 
         output$Forecast<- renderPlot({
+
+            test = checkyears(input$years,FALSE)
+
             inp = input$pollutant
             poll = subset(test,subset= inquinante==inp)
             poll= poll[,c('data','valore')]
@@ -150,16 +171,13 @@ loadlibreries <- function()
     require(httr)
     require(jsonlite)
     require(tidyverse)
-   # require(shinydashboard)
-#    require(leaflet)
 
 }
 
-scraping <- function()
+scraping <- function(id)
 {
     url <- paste0("http://dati.comune.milano.it/api/action/",
-                  "datastore_search?",
-                  "resource_id=698a58e6-f276-44e1-92b1-3d2b81a4ad47&limit=10000")
+                  "datastore_search?", paste("resource_id=", id,"&limit=10000", sep=""))
     page <- GET(url) # API request
     status_code(page) # # Check that the call is successful
     leggo_list <- fromJSON(url)
@@ -205,6 +223,38 @@ datacleaning <- function(leggo)
     return (test)
 }
 
+checkyears  <- function(year, flat)
+{
+
+    if(flat)
+    {
+        if(year=="2019")
+        {
+            return(flat_ds2019)
+        }
+        if(year=="2018")
+        {
+            return(flat_ds2018)
+        }
+        if(year=="2017")
+        {
+            return(flat_ds2017)
+        }
+    }
+    if(year=="2019")
+    {
+        return(ds2019)
+    }
+    if(year=="2018")
+    {
+        return(ds2018)
+    }
+    if(year=="2017")
+    {
+        return(ds2017)
+    }
+
+}
 
 installpack <- function()
 {
@@ -215,3 +265,4 @@ installpack <- function()
 }
 
 #milanpollution()
+
