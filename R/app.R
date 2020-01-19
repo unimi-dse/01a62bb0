fdbe6df6d0f38d8085d95bd623e86d1835c9eb95
Andreaierardi <andreierardi@gmail.com>
2020-01-19 13:18:51 +0100

    uploading first version

---
+++ R/app.R
@@ -0,0 +1,117 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+run <- function() {
+library(shiny)
+library(ggplot2)
+library(forecast)
+library(xts)
+library(ckanr)
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+
+url <- paste0("http://dati.comune.milano.it/api/action/",
+              "datastore_search?",
+              "resource_id=698a58e6-f276-44e1-92b1-3d2b81a4ad47&limit=10000")
+page <- GET(url) # API request
+status_code(page) # # Check that the call is successful
+leggo_list <- fromJSON(url)
+leggo <- leggo_list$result$records
+
+Data = leggo
+Data$stazione_id=NULL
+Data$`_id`=NULL
+Data$valore = as.double(Data$valore)
+Data$inquinante = as.factor(Data$inquinante)
+Data = Data[complete.cases(Data),]
+Data$data = as.Date(Data$data)
+test = aggregate(valore~ data+inquinante, Data , mean)
+
+
+
+# Define UI for application that draws a histogram
+
+ui <- fluidPage(
+    titlePanel("Milan pollution 2019"),
+    sidebarLayout(
+        sidebarPanel(
+            # Dropdown menu for selecting variable from GE data.
+            selectInput("pollutant",
+                        label = "Select pollutant type",
+                        choices = unique(test$inquinante),
+                        selected = "PM10"),
+            hr(),
+            helpText("Data from openData comune di Milano"),
+            # Default selection
+            sliderInput("lag",
+                        "Forecasting lags:",
+                        min = 1,  max = 50, value = 1),
+        ),
+        mainPanel(
+            plotOutput("BarPlot"),
+            plotOutput("Timeseries"),
+            plotOutput("Forecast")
+
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+
+    output$BarPlot <- renderPlot({
+        inp = input$pollutant
+
+        poll = subset(test,subset= inquinante==inp)
+        poll= poll[,c('data','valore')]
+        ggplot(poll, aes(x=data,y=valore, group=1))+geom_point()+geom_line()+ geom_smooth(method="lm")
+
+
+
+        # Render a barplot
+    })
+
+
+    output$Timeseries <- renderPlot({
+        inp = input$pollutant
+
+        poll = subset(test,subset= inquinante==inp)
+        poll= poll[,c('data','valore')]
+        ggplot(poll, aes(x=data,y=valore, group=1))+geom_point()+geom_line()+ geom_smooth(method="lm")
+
+        time = poll$data = NULL
+        time.ts = as.ts(poll)
+        plot(time.ts)
+# Render a barplot
+
+    })
+
+    output$Forecast<- renderPlot({
+        inp = input$pollutant
+
+        poll = subset(test,subset= inquinante==inp)
+        poll= poll[,c('data','valore')]
+        ggplot(poll, aes(x=data,y=valore, group=1, color=valore))+geom_point()+geom_line()+ geom_smooth(method="lm")
+
+        time = poll$data = NULL
+        time.ts = as.ts(poll)
+        fit = auto.arima(time.ts)
+
+
+        # Render a forecast plot
+        plot(forecast(fit,input$lag))
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+}
