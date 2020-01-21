d1e27227bd9670321dfbcd4b4448d4f559f49bc0
Andrea Ierardi <andreierardi@gmail.com>
2020-01-21 11:34:07 +0100

    update css colours

--- R/milanpollution.R
+++ R/milanpollution.R
@@ -146,7 +146,7 @@ milanpollution <- function()
           h1(
       id="big-heading",
              "Milan Pollution Analisys")),
-      tags$style(HTML("#backg{border:1px solid black;background-color:#ecf8f2; margin: 5px 5px 5px 5px;} #navb{background-color:#49c5b6;color:#ffffff} #side{background-color:#f4f5f9;} #big-heading{ color: #4990c2; font-family: 'Raleway',sans-serif; font-size: 48px; font-weight: 600; line-height: 27px; margin: 0 0 10px;text-align:center;background-color:#ffffff;}")),
+      tags$style(HTML("#backg{border:1px solid black;background-color:#ecf8f2; margin-bottom: 10px;} #navb{background-color:#49c5b6;color:#ffffff} #side{background-color:#f4f5f9;} #big-heading{ color: #4990c2; font-family: 'Raleway',sans-serif; font-size: 48px; font-weight: 600; line-height: 27px; margin: 0 0 10px;text-align:center;background-color:#ffffff;}")),
 
         hr(),
         navbarPage( "Pollution Milan",
@@ -212,7 +212,7 @@ milanpollution <- function()
                  mainPanel(id="backg",
                             h1("Plot results"),
                            plotlyOutput("stations_plot"),
-
+                            br(),
                  ))
 
                  ),
